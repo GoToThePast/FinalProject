bc795357a1624ccd7cba34b5238f8c785ddef31b
IAmTrying <thetrying@qq.com>
2021-01-15 15:19:36 +0800

    update

--- src/com/tuogen/filter/EncodingFilter.java
+++ src/com/tuogen/filter/EncodingFilter.java
@@ -9,22 +9,21 @@ import java.io.IOException;
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
     private String encoding;
-    private static final String DEFAULT_CHARSET = "UTF-8";
+    private static final String DEFAULT_CHARSET = "utf-8";
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response=(HttpServletResponse) resp;
         //根据请求方式不同设置编码
         if ("GET".equals(request.getMethod())){
             EncodingWrapper wrapper = new EncodingWrapper(request, encoding);
-            response.setContentType("text/html;charset="+encoding);
+            request.setCharacterEncoding(encoding);
             chain.doFilter(wrapper,resp);
         }else{
             //设置post方式编码
             request.setCharacterEncoding(encoding);
-            response.setContentType("text/html;charset="+encoding);
+            resp.setContentType("text/html;charset="+encoding);
             chain.doFilter(req, resp);
         }
     }
