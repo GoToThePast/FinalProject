fba268e3503063f8085ff1408a155408744a67a2
IAmTrying <thetrying@qq.com>
2021-01-15 13:44:24 +0800

    update

--- src/com/tuogen/filter/EncodingFilter.java
+++ src/com/tuogen/filter/EncodingFilter.java
@@ -2,19 +2,38 @@ package com.tuogen.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "EncodingFilter")
+@WebFilter("/*")
 public class EncodingFilter implements Filter {
+    private String encoding;
+    private static final String DEFAULT_CHARSET = "UTF-8";
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response=(HttpServletResponse) resp;
+        //根据请求方式不同设置编码
+        if ("GET".equals(request.getMethod())){
+            EncodingWrapper wrapper = new EncodingWrapper(request, encoding);
+            response.setContentType("text/html;charset="+encoding);
+            chain.doFilter(wrapper,resp);
+        }else{
+            //设置post方式编码
+            request.setCharacterEncoding(encoding);
+            response.setContentType("text/html;charset="+encoding);
+            chain.doFilter(req, resp);
+        }
     }
 
     public void init(FilterConfig config) throws ServletException {
-
+        //获取配置中的编码
+        encoding = config.getInitParameter("encoding");
+        if(encoding==null||"".equals(encoding))
+            encoding=DEFAULT_CHARSET;
     }
 
 }
--- src/com/tuogen/filter/EncodingWrapper.java
+++ src/com/tuogen/filter/EncodingWrapper.java
@@ -1,4 +1,26 @@
 package com.tuogen.filter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.UnsupportedEncodingException;
+public class EncodingWrapper extends HttpServletRequestWrapper {
+    private HttpServletRequest request;
+    private String encoding;
+    public EncodingWrapper(HttpServletRequest request,String encoding) {
+        super(request);
+        this.request=request;
+        this.encoding=encoding;
+    }
 
-public class EncodingWrapper {
+    @Override
+    public String getParameter(String name) {
+        String value = request.getParameter(name);
+        if (value!=null){
+            try {
+                value = new String(value.getBytes("iso8859-1"), encoding);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return value;
+    }
 }
