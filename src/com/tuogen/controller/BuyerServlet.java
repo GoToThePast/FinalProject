d57aac8f2b1723851d721875a8a59119baf46849
IAmTrying <thetrying@qq.com>
2021-01-18 14:34:08 +0800

    asd

--- src/com/tuogen/controller/BuyerServlet.java
+++ src/com/tuogen/controller/BuyerServlet.java
@@ -93,7 +93,7 @@ public class BuyerServlet extends BaseServlet {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         String date = df.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
         OnlineUser onlineUser=new OnlineUser(Type.BUYER,buyer,date);
-        session.setAttribute("onlinerUser",onlineUser);
+        session.setAttribute("onlineUser",onlineUser);
     }
 
     private Buyer checkIdentity(String userAccout, String userPwd) {
